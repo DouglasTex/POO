fcf384593ae4fd0546d689a215a3b34704ac3229
douglasTex <douglas.cardins@gmail.com>
2017-08-03 16:38:21 -0300

    Inicio da questão 4

---
+++ Documents/workspace/POO/src/Questao4/Bits.java
@@ -0,0 +1,32 @@
+package Questao4;
+
+import java.util.List;
+
+public class Bits {
+	
+	List[] listBits;
+	
+	public Bits(int tamanho) {
+		this.listBits = new List [tamanho];
+	}
+
+	
+	
+	public Integer getBit() {
+		
+		return null;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+}
